33cf44bd089f738036273bc7307da87041958ca0
Daniel <jimenezguerrerojesusdaniel@gmail.com>
2026-04-22 15:17:06 -0700

    Se añadio la pagina de Usuarios

--- ActividadReact/src/App.jsx
+++ ActividadReact/src/App.jsx
@@ -1,9 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import './index.css';
  import Home from "./pages/Home";
 import Acerca from "./pages/Acerca.jsx";
 // import Contacto from "./pages/Contacto";
 import Tablas from "./pages/Tablas.jsx";
 import Navbar from "./components/Navbar";
+import Usuarios from "./pages/Usuarios";
+
+
+//importar de Usuario.jsx a App.jsx
 
 function App() {
   return (
@@ -15,6 +20,8 @@ function App() {
         <Route path="/acerca" element={<Acerca />} />
         {/* <Route path="/contacto" element={<Contacto />} /> */}
         <Route path="/tablas" element={<Tablas />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        
       </Routes>
     </>
   );
